6a4a7fa749d664d17836bcbac1a25020173a5607
Iman Gure <imangu99@gmail.com>
2019-06-07 09:06:02 -0700

    still working on js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,3 +7,8 @@ $(document).ready(function () {
     var correct = 0;
     var incorrect = 0;
     var unanswered = 0;
+
+    // Questions and Answer Arrays
+    var question = ["Is an Asian country with no bordering countries?",
+        "Is a Nordic country?", "Has a crescent moon on its flag?", "Lies on the Prime Meridian?", "Begins with the letter 'R'?",
+        "Has the same name as its capital?", "Was a member of the former Yugoslavia?", "Is a Communist country?"];
