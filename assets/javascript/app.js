f2e0d3ee30e222cf78d3376a13f3ebbd4d6a9d33
Iman Gure <imangu99@gmail.com>
2019-06-07 08:59:59 -0700

    updating file

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// Define variables
+$(document).ready(function () {
+    var count = 0;
+    var time = 31;
+    var isSelected = false;
+    var ticker;
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
